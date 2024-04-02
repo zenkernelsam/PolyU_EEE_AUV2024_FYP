2e97521e9269dde72347e21816ab006cf50c64b3
Cisco He <hejiajie9527@gmail.com>
2024-04-02 11:41:20 +0800

    Update auv.launch.py

--- src/start_auv/launch/auv.launch.py
+++ src/start_auv/launch/auv.launch.py
@@ -1,16 +1,8 @@
-from launch import LaunchDescription           # launch文件的描述类
-from launch_ros.actions import Node            # 节点启动的描述类
+from launch import LaunchDescription          
+from launch_ros.actions import Node          
 
-def generate_launch_description():             # 自动生成launch文件的函数
-    return LaunchDescription([                 # 返回launch文件的描述信息
-        #Node(                                  # 配置一个节点的启动
-        #    package='facer_input',             # 节点所在的功能包
-        #    executable='capture_image',        # 节点的可执行文件
-        #),
-        #Node(                                  # 配置一个节点的启动
-        #    package='task1_flare_detection',   # 节点所在的功能包
-        #    executable='flare_detection',      # 节点的可执行文件
-        #),
+def generate_launch_description():            
+    return LaunchDescription([                 
         Node(
             package='buoyancy_node',
             executable='buoyancy_node_test',      
